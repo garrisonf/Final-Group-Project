a57e6ccedc65d18c5cf8bd33d9db8604c702d9c3
egilssone <62882468+egilssone@users.noreply.github.com>
2020-06-06 05:38:23 -0700

    Fixed AbilityScore
    Brad seems to have accidentally deleted all of the code in this class, this fixes that by putting the code back.

--- AbilityScore.java
+++ AbilityScore.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DungeonsAndDragons;
 
-/**
- *
- * @author bvill
- */
-public class AbilityScore {
+
+import java.io.Serializable;
+//@author: egilsson
+
+public class AbilityScore implements Serializable {
+    private int value;
+    private int mod;
     
+    AbilityScore(int value) {
+        this.value = value;
+        this.mod = calculateMod(value);
+    }
+    
+    public int calculateMod(int stat) {
+            if(stat >= 1 && stat <=30)
+            return stat/2 - 5;
+        else if(stat < 1) 
+            return -5;
+        else 
+            return 10;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public void setValue(int value) {
+        if(value < 0) value = 0;
+        this.value = value;
+        this.mod = calculateMod(value);
+    }
+    
+    public int getMod() {
+        return mod;
+    }
+    
+    @Override
+    public String toString() {
+        String pos = "";
+        if(mod >= 0) pos = "+";
+        return value + "("+ pos + mod + ")";
+    }
 }
